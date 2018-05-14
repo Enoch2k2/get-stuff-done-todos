3bd5117e10128206d426b66106807f403d7942b5
Enoch2k2 <enoch2k2@hotmail.com>
2018-05-14 11:21:04 -0500

    added user show page, authenticated user to not display root path if logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
-  def index
+  before_action :authenticate_user, only: [:index]
 
+  def index
   end
+
+  private
+    def authenticate_user
+      redirect_to user_path(current_user) if user_signed_in?
+    end
 end
